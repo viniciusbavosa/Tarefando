19effd1344464023d79feb9839a10d7b4bb19261
viniciusbavosa <viniciusbavosa@outlook.com>
2024-04-24 10:01:47 -0300

    refactor: implement .env and tests

--- tests/selenium/frontend/send_task_test.js
+++ tests/selenium/frontend/send_task_test.js
@@ -4,10 +4,9 @@ const driver_chr = await new Builder().forBrowser(Browser.CHROME).build();
 const driver_fx = await new Builder().forBrowser(Browser.FIREFOX).build();
 
 async function sendTaskTest(driver) {
-
   try {
   
-    await driver.get('http://localhost:80/');
+    await driver.get('http://localhost:5000/');
     await driver.manage().window().setRect({ width: 639, height: 800 }); // Configura o tamanho da tela
     await driver.navigate().refresh(); //Atualiza a página
     const button = driver.findElement(By.className('new-task-bttn-container'));
